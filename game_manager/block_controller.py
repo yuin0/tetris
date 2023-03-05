d0ee5adf1f196c05399e9fa19a20333ef9873cab
yuin0 <yuinoforSK@gmail.com>
2023-03-05 16:58:20 +0900

    chore: remove libraries not needed

--- game_manager/block_controller.py
+++ game_manager/block_controller.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime  # To pick up date
-from operator import mul  # To multiple lists
 import pprint  # To customize output
 import copy  # To copy objects
 import numpy as np
